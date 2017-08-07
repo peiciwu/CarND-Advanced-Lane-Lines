60c12e49fb9f641aba53a1de466f21a0f4d19730
Pei-Ci Wu <peiciwu@gmail.com>
2017-08-07 01:25:51 -0700

    Finished thresholding. Added initial version of warped images.
    	modified:   process_image.py

--- process_image.py
+++ process_image.py
@@ -22,6 +22,12 @@ def plotTwo(image, title, colorMap=('',''), saveName=''):
 def undistort(img):
     return cv2.undistort(img, mtx, dist, None, mtx);
 
+# Create a binary image of ones where threshold is met, zeros otherwise
+def binary_thresh(channel, thresh=(0, 255)):
+    binary_output = np.zeros_like(channel)
+    binary_output[(channel >= thresh[0]) & (channel <= thresh[1])] = 1
+    return binary_output
+
 # Applies Sobel x or y, then takes an absolute value and applies a threshold.
 # NOTE the given image must be grayscale.
 def abs_sobel_thresh(gray, orient='x', sobel_kernel=3, thresh=(0, 255)):
@@ -29,10 +35,7 @@ def abs_sobel_thresh(gray, orient='x', sobel_kernel=3, thresh=(0, 255)):
     abs_sobel = np.absolute(sobel)
     # Scale to 8-bit and convert to type uint8
     scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
-    # Create a binary image of ones where threshold is met, zeros otherwise
-    binary_output = np.zeros_like(scaled_sobel)
-    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
-    return binary_output
+    return binary_thresh(scaled_sobel, thresh)
 
 # Computes the magnitude of the gradient, and applies a threshold.
 # NOTE the given image must be grayscale.
@@ -42,10 +45,7 @@ def mag_thresh(gray, sobel_kernel=3, thresh=(0, 255)):
     mag = np.sqrt(sobelx**2 + sobely**2) # Magnitude of the gradient
     # Scale to 8-bit and convert to type uint8
     scaled_mag = np.uint8(255 * mag / np.max(mag))
-    # Create a binary image of ones where threshold is met, zeros otherwise
-    binary_output = np.zeros_like(scaled_mag)
-    binary_output[(scaled_mag >= thresh[0]) & (scaled_mag <= thresh[1])] = 1
-    return binary_output
+    return binary_thresh(scaled_mag, thresh)
 
 # Computes the direction of the graident, and applies a threshold.
 # NOTE the given image must be grayscale.
@@ -54,18 +54,33 @@ def dir_thesh(gray, sobel_kerne=3, thresh=(0, np.pi/2)):
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     # Take absolute of sobelx and sobely to get the gradient direction
     grad_dir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    # Create a binary image of ones where threshold is met, zeros otherwise
-    binary_output = np.zeros_like(grad_dir)
-    binary_output[(grad_dir >= thresh[0]) & (grad_dir <= thresh[1])] = 1
-    return binary_output
+    return binary_thresh(grad_dir, thresh)
 
-# Applies a threshold on the S channel of HLS color space.
-# NOTE the given image must be on HLS color space.
-def hls_s_thresh(hls, thresh=(0, 255)):
-    s_channel = hls[:, :, 2]
-    binary_output = np.zeros_like(s_channel)
-    binary_output[(s_channel >= thresh[0]) & (s_channel <= thresh[1])] = 1
-    return binary_output
+# Main function to combine color and gradient thresholds
+def thresholding(img, plot = False):
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    s_channel = hls[:,:,2]
+    r_channel = img[:,:,0]
+
+    sobelx_binary = abs_sobel_thresh(gray, 'x', 3, (30, 150))
+    s_binary = binary_thresh(s_channel, (175, 250)) # On s-channel
+    r_binary = binary_thresh(r_channel, (230, 255)) # On r-channel
+
+    combined_binary = np.zeros_like(sobelx_binary)
+    combined_binary[(s_binary == 1) | (sobelx_binary == 1) | (r_binary == 1)] = 1
+
+    if plot == True:
+        plotTwo((gray, sobelx_binary), ('gray', 'sobelx'), ('gray', 'gray'))
+        plotTwo((s_channel, s_binary), ('s-channel', 's-threshold'), ('gray', 'gray'))
+        plotTwo((r_channel, r_binary), ('r-channel', 'r-threshold'), ('gray', 'gray'))
+        plotTwo((img, combined_binary), ('undistorted', 'combined'), ('', 'gray'))
+
+    return combined_binary
+
+def perspective_tranform(img):
+    img_size 
+
 
 # Read in the saved camera calibration matrix and distortion cofficients
 dist_pickle = pickle.load(open("calibration.p", "rb"))
@@ -76,33 +91,28 @@ dist = dist_pickle["dist"]
 testFiles = glob.glob('./test_images/*.jpg')
 
 """
-# Original images
-orig_images = []
+# Process all the images: distort, thresholding, and perspective transform
 for fname in testFiles:
-    orig_images.append(mpimg.imread(fname))
-# Undistorted images
-undistorted_images = []
-for img in orig_images:
+    img = mpimg.imread(fname)
     undist = undistort(img)
-    undistorted_images.append(undist)
-    #plotTwo(img, undist, 'original', 'undistorted')
-
-example = len(orig_images) - 2;
-plotTwo(orig_images[example], undistorted_images[example], 'original', 'undistorted', 'undistorted.png');
+    plotTwo((img, undist), ('original', 'undistorted'))
+    thresh = thresholding(undist, plot=True)
 """
 
+
+
 # FIXME: pw test color and threshold
-example = mpimg.imread('./test_images/test5.jpg');
-img = undistort(example)
-plotTwo((example, img), ('original', 'undistorted'))
+#example = mpimg.imread('./test_images/test5.jpg');
+example = mpimg.imread('./test_images/straight_lines1.jpg');
+undist = undistort(example)
+plotTwo((example, undist), ('original', 'undistorted'))
+thresh = thresholding(undist, plot=False)
+plotTwo((undist, thresh), ('undistorted', 'thresholding'), ('', 'gray'))
 
-hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+h, w = thresh.shape[:2]
+src = np.float32([(575,464), (707,464), (258,682), (1049,682)])
+dst = np.float32([(300,0), (w-300,0), (300,h), (w-300,h)])
+M = cv2.getPerspectiveTransform(src, dst)
+warped = cv2.warpPerspective(thresh, M, (w, h))
+plotTwo((thresh, warped), ('thresholding', 'warped'), ('gray', 'gray'))
 
-sobelx_binary = abs_sobel_thresh(gray, 'x', 3, (20, 100))
-s_binary = hls_s_thresh(hls, (170, 255))
-
-combined_binary = np.zeros_like(sobelx_binary)
-combined_binary[(s_binary == 1) | (sobelx_binary == 1)] = 1
-
-plotTwo((img, combined_binary), ('undistorted', 'combined_1'), ('', 'gray'), 'combined_1.png')
