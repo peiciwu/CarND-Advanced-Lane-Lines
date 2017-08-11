c91e63aa6c5d7152c59ba574d2112c6d77462cd6
Pei-Ci Wu <peiciwu@gmail.com>
2017-08-11 02:28:18 -0700

    Code refactor. Finalize parameters for transform_perspective().
    Will be further refactored.
    
    	new file:   color_thresh_utility.py
    	modified:   process_image.py

---
+++ color_thresh_utility.py
@@ -0,0 +1,137 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+import glob
+
+class ImageProcessor:
+    def __init__(self):
+        # Read in the saved camera calibration matrix and distortion cofficients
+        dist_pickle = pickle.load(open("calibration.p", "rb"))
+        self.mtx = dist_pickle["mtx"]
+        self.dist = dist_pickle["dist"]
+        self.src, self.dst, self.M, self.invM = self.get_perspective_transform_parameters()
+
+    def undistort(self, img):
+        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx);
+
+    # Main function to combine color and gradient thresholds
+    def thresholding(self, img, plot = False):
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        s_channel = hls[:,:,2]
+        r_channel = img[:,:,0]
+
+        sobelx_binary = abs_sobel_thresh(gray, 'x', 3, (30, 150))
+        #mag_binary = mag_thresh(gray, 9, (50, 255))
+        #dir_binary = dir_thresh(gray, 15, (0.7, 1.3))
+        s_binary = binary_thresh(s_channel, (175, 250)) # On s-channel
+        r_binary = binary_thresh(r_channel, (230, 255)) # On r-channel
+
+        combined_binary = np.zeros_like(sobelx_binary)
+        combined_binary[(s_binary == 1) | (sobelx_binary == 1) | (r_binary == 1)] = 1
+        #combined_binary[((mag_binary == 1) & (dir_binary == 1)) | (s_binary == 1) | (r_binary == 1)] = 1
+
+        if plot == True:
+            plotTwo((gray, sobelx_binary), ('gray', 'sobelx'), ('gray', 'gray'))
+            plotTwo((s_channel, s_binary), ('s-channel', 's-threshold'), ('gray', 'gray'))
+            plotTwo((r_channel, r_binary), ('r-channel', 'r-threshold'), ('gray', 'gray'))
+            plotTwo((img, combined_binary), ('undistorted', 'combined'), ('', 'gray'))
+
+        return combined_binary
+
+    def perspective_transform(self, img, plot = False):
+        warped = cv2.warpPerspective(img, self.M, (img.shape[1], img.shape[0]))
+        if plot == True:
+            img_region = cv2.polylines(img, [np.int32(self.src)], True, (255, 0, 0), 3)
+            warped_region = cv2.polylines(warped, [np.int32(self.dst)], True, (255, 0, 0), 3)
+            plotTwo((img_region, warped_region), ('img_with_region', 'warped_with_region'))
+        return warped
+
+    # Return (src, dst, M, invM).
+    # Start roughly from 4 points on the two lines at straight_lines1.jpg (two
+    # points on one line), and then perform some minor adjust based on th warped results.
+    def get_perspective_transform_parameters(self):
+        example = mpimg.imread('./test_images/straight_lines1.jpg');
+        undist = self.undistort(example)
+        # Get perspective transform matrix
+        h, w = undist.shape[:2]
+        # Two lines on the "straight_lines1.jpg": (216, 705), (585, 454), (687, 450), (1073, 693)
+        slope1 = (705-454)/(216-585);
+        slope2 = (450-693)/(687-1073)
+        y = 460
+        #x0 = 220
+        x0 = 203
+        x4 = 1122
+        #x1 =  (y-h)/slope1+x0 - 15
+        x1 =  (y-h)/slope1+x0 - 3
+        x2 = x4 - (h-y)/slope2 - 3
+        #print ("x1: ", x1, ", x2: ", x2)
+
+        src = np.float32([(x0,h), (x1,y), (x2,y), (x4,h)])
+        dst = np.float32([(250,h), (250,0), (w-300,0), (w-300, h)])
+        M = cv2.getPerspectiveTransform(src, dst)
+        invM = cv2.getPerspectiveTransform(dst, src)
+
+        return (src, dst, M, invM)
+
+# Create a binary image of ones where threshold is met, zeros otherwise
+def binary_thresh(channel, thresh=(0, 255)):
+    binary_output = np.zeros_like(channel)
+    binary_output[(channel >= thresh[0]) & (channel <= thresh[1])] = 1
+    return binary_output
+
+# Applies Sobel x or y, then takes an absolute value and applies a threshold.
+# NOTE the given image must be grayscale.
+def abs_sobel_thresh(gray, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    sobel = cv2.Sobel(gray, cv2.CV_64F, orient=='x', orient=='y', ksize=sobel_kernel) # Take the dervivative either in x or y
+    abs_sobel = np.absolute(sobel)
+    # Scale to 8-bit and convert to type uint8
+    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+    return binary_thresh(scaled_sobel, thresh)
+
+# Computes the magnitude of the gradient, and applies a threshold.
+# NOTE the given image must be grayscale.
+def mag_thresh(gray, sobel_kernel=3, thresh=(0, 255)):
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    mag = np.sqrt(sobelx**2 + sobely**2) # Magnitude of the gradient
+    # Scale to 8-bit and convert to type uint8
+    scaled_mag = np.uint8(255 * mag / np.max(mag))
+    return binary_thresh(scaled_mag, thresh)
+
+# Computes the direction of the graident, and applies a threshold.
+# NOTE the given image must be grayscale.
+def dir_thresh(gray, sobel_kernel=3, thresh=(0, np.pi/2)):
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take absolute of sobelx and sobely to get the gradient direction
+    grad_dir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    return binary_thresh(grad_dir, thresh)
+
+# Helper function for displaying two images in parallel to compare.
+def plotTwo(image, title, colorMap=('',''), saveName=''):
+    fig, axis = plt.subplots(1, 2, figsize=(12, 10))
+    fig.tight_layout()
+    for i in range(2):
+        if (colorMap[i] == ''):
+            axis[i].imshow(image[i])
+        else:
+            axis[i].imshow(image[i], cmap=colorMap[i])
+        axis[i].set_title(title[i])
+    if saveName != '':
+        plt.savefig(saveName, bbox_inches='tight')
+    plt.show(block=True)
+
+"""
+# Verify perspective_transform:
+# - The lines on the image warped from straight_lines1.jpg should be vertical. 
+# - The curvel lines should be parallel to each other.
+image_processor = ImageProcessor()
+testFiles = glob.glob('./test_images/*.jpg')
+for fname in testFiles:
+    img = mpimg.imread(fname)
+    undist = image_processor.undistort(img)
+    warped = image_processor.perspective_transform(undist, plot=True)
+"""
--- process_image.py
+++ process_image.py
@@ -3,90 +3,8 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
-import pickle
 import os
-
-# Helper function for displaying two images in parallel to compare.
-def plotTwo(image, title, colorMap=('',''), saveName=''):
-    fig, axis = plt.subplots(1, 2, figsize=(12, 10))
-    fig.tight_layout()
-    for i in range(2):
-        if (colorMap[i] == ''):
-            axis[i].imshow(image[i])
-        else:
-            axis[i].imshow(image[i], cmap=colorMap[i])
-        axis[i].set_title(title[i])
-    if saveName != '':
-        plt.savefig(saveName, bbox_inches='tight')
-    plt.show(block=True)
-
-def undistort(img):
-    return cv2.undistort(img, mtx, dist, None, mtx);
-
-# Create a binary image of ones where threshold is met, zeros otherwise
-def binary_thresh(channel, thresh=(0, 255)):
-    binary_output = np.zeros_like(channel)
-    binary_output[(channel >= thresh[0]) & (channel <= thresh[1])] = 1
-    return binary_output
-
-# Applies Sobel x or y, then takes an absolute value and applies a threshold.
-# NOTE the given image must be grayscale.
-def abs_sobel_thresh(gray, orient='x', sobel_kernel=3, thresh=(0, 255)):
-    sobel = cv2.Sobel(gray, cv2.CV_64F, orient=='x', orient=='y', ksize=sobel_kernel) # Take the dervivative either in x or y
-    abs_sobel = np.absolute(sobel)
-    # Scale to 8-bit and convert to type uint8
-    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
-    return binary_thresh(scaled_sobel, thresh)
-
-# Computes the magnitude of the gradient, and applies a threshold.
-# NOTE the given image must be grayscale.
-def mag_thresh(gray, sobel_kernel=3, thresh=(0, 255)):
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    mag = np.sqrt(sobelx**2 + sobely**2) # Magnitude of the gradient
-    # Scale to 8-bit and convert to type uint8
-    scaled_mag = np.uint8(255 * mag / np.max(mag))
-    return binary_thresh(scaled_mag, thresh)
-
-# Computes the direction of the graident, and applies a threshold.
-# NOTE the given image must be grayscale.
-def dir_thesh(gray, sobel_kerne=3, thresh=(0, np.pi/2)):
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    # Take absolute of sobelx and sobely to get the gradient direction
-    grad_dir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    return binary_thresh(grad_dir, thresh)
-
-# Main function to combine color and gradient thresholds
-def thresholding(img, plot = False):
-    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    s_channel = hls[:,:,2]
-    r_channel = img[:,:,0]
-
-    sobelx_binary = abs_sobel_thresh(gray, 'x', 3, (30, 150))
-    s_binary = binary_thresh(s_channel, (175, 250)) # On s-channel
-    r_binary = binary_thresh(r_channel, (230, 255)) # On r-channel
-
-    combined_binary = np.zeros_like(sobelx_binary)
-    combined_binary[(s_binary == 1) | (sobelx_binary == 1) | (r_binary == 1)] = 1
-
-    if plot == True:
-        plotTwo((gray, sobelx_binary), ('gray', 'sobelx'), ('gray', 'gray'))
-        plotTwo((s_channel, s_binary), ('s-channel', 's-threshold'), ('gray', 'gray'))
-        plotTwo((r_channel, r_binary), ('r-channel', 'r-threshold'), ('gray', 'gray'))
-        plotTwo((img, combined_binary), ('undistorted', 'combined'), ('', 'gray'))
-
-    return combined_binary
-
-# Apply perspective transform using the given matrix/source/destination points.
-def perspective_transform(img, plot = False):
-    warped = cv2.warpPerspective(img, p_M, (img.shape[1], img.shape[0]))
-    if plot == True:
-        img_region = cv2.polylines(img, [np.int32(p_src)], True, (255, 0, 0), 3)
-        warped_region = cv2.polylines(warped, [np.int32(p_dst)], True, (255, 0, 0), 3)
-        plotTwo((img_region, warped_region), ('img_with_region', 'warped_with_region'))
-    return warped
+from color_thresh_utility import ImageProcessor, plotTwo
 
 def run_all_test_images():
     # List of all the test images
@@ -95,16 +13,15 @@ def run_all_test_images():
     # Process all the images: distort, thresholding, and perspective transform
     for fname in testFiles:
         img = mpimg.imread(fname)
-        undist = undistort(img)
+        undist = image_processor.undistort(img)
         name = '(' + os.path.splitext(os.path.basename(fname))[0] + ')'
         plotTwo((img, undist), ('Original'+name, 'Undistorted'+name))
-        thresh = thresholding(undist)
+        thresh = image_processor.thresholding(undist)
         plotTwo((undist, thresh), ('Undistorted'+name, 'Thresholding'+name), ('', 'gray'))
-        perspective_transform(undist, plot=True)
-        warped = perspective_transform(thresh)
+        warped = image_processor.perspective_transform(thresh)
         plotTwo((thresh, warped), ('Thresholding'+name, 'Warped'+name), ('gray', 'gray'))
-        left_fitx, right_fitx, ploty = find_lane_lines(warped, plot=True)
-        result = draw_lane(img, warped, left_fitx, right_fitx, ploty, plot=True)
+        left_fit, right_fit = find_lane_lines(warped, plot=True)
+        result = draw_lane(img, warped, left_fit, right_fit, plot=True)
 
 #FIXME: findling the line 
 def find_lane_lines(img, plot=False):
@@ -115,7 +32,8 @@ def find_lane_lines(img, plot=False):
     min_pixels = 50 # minimum number of pixels found in one window
 
     # Create an output image to draw on and  visualize the result
-    out_img = np.dstack((img, img, img))*255
+    if plot == True:
+        out_img = np.dstack((img, img, img))*255
 
     # Starting points: The peaks of the histogram at the left half and the right half.
     histogram = np.sum(img[img.shape[0]//2:,:], axis=0)
@@ -162,10 +80,6 @@ def find_lane_lines(img, plot=False):
     left_fit = np.polyfit(nonzeroy[left_lane_indices], nonzerox[left_lane_indices], 2)
     right_fit = np.polyfit(nonzeroy[right_lane_indices], nonzerox[right_lane_indices], 2)
 
-    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
-    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
     if plot == True:
         # Mark the pixels in the window: left with red, right with blue
         out_img[nonzeroy[left_lane_indices], nonzerox[left_lane_indices]] = [255, 0, 0]
@@ -173,6 +87,9 @@ def find_lane_lines(img, plot=False):
         plt.imshow(out_img)
 
         # Plot the fitted polynomial
+        ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
         plt.plot(left_fitx, ploty, color='yellow')
         plt.plot(right_fitx, ploty, color='yellow')
         plt.xlim(0, img.shape[1])
@@ -180,11 +97,67 @@ def find_lane_lines(img, plot=False):
 
         plt.show(block=True)
     
-    return left_fitx, right_fitx, ploty
+    return left_fit, right_fit
 
-def draw_lane(img, warped, left_fitx, right_fitx, ploty, plot=False):
+def find_lane_lines_using_previous_fit(img, left_fit, right_fit, plot=False):
+    # X and y positions where the pixels are nonzero
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100 # widnow width = 2 * margin
+    # Indices within the margin of the polynomial
+    left_lane_indices = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2]) - margin) & 
+                         (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2]) + margin))
+    right_lane_indices = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2]) - margin) & 
+                          (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2]) + margin))
+    # Fit a second order polynomial
+    left_fit = np.polyfit(nonzeroy[left_lane_indices], nonzerox[left_lane_indices], 2)
+    right_fit = np.polyfit(nonzeroy[right_lane_indices], nonzerox[right_lane_indices], 2)
+
+    # Create an output image to draw on and  visualize the result
+    if plot == True:
+        out_img = np.dstack((img, img, img))*255
+        # Mark the pixels in the window: left with red, right with blue
+        out_img[nonzeroy[left_lane_indices], nonzerox[left_lane_indices]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_indices], nonzerox[right_lane_indices]] = [0, 0, 255]
+        plt.imshow(out_img)
+
+	# Generate a polygon to illustrate the search window area
+        ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+        
+        # And recast the x and y points into usable format for cv2.fillPoly()
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
+        right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+        # window img
+        window_img = np.zeros_like(out_img)
+        # Draw the lane onto the window image
+        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+        plt.imshow(result)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, img.shape[1])
+        plt.ylim(img.shape[0], 0)
+
+        plt.show(block=True)
+
+    return left_fit, right_fit
+
+def draw_lane(img, warped, left_fit, right_fit, plot=False):
     warp_zero = np.zeros_like(warped).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
     # Recast the x and y points into usable format for cv2.fillPoly()
     pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
@@ -198,10 +171,9 @@ def draw_lane(img, warped, left_fitx, right_fitx, ploty, plot=False):
 
     # Warp the blank back to original image space using inverse perspective matrix (Minv)
     #FIXME: should store
-    Minv = cv2.getPerspectiveTransform(p_dst, p_src)
-    new_warp = cv2.warpPerspective(color_warp, Minv, (img.shape[1], img.shape[0])) 
+    new_warp = cv2.warpPerspective(color_warp, image_processor.invM, (img.shape[1], img.shape[0])) 
     # Combine the result with the original image
-    result = cv2.addWeighted(img, 1, new_warp, 0.5, 0)
+    result = cv2.addWeighted(img, 1, new_warp, 0.3, 0)
 
     if plot == True:
         plt.imshow(result)
@@ -209,64 +181,60 @@ def draw_lane(img, warped, left_fitx, right_fitx, ploty, plot=False):
 
     return result
 
-def process_image(img):
-    undist = undistort(img)
-    thresh = thresholding(undist)
-    warped = perspective_transform(thresh)
-    left_fitx, right_fitx, ploty = find_lane_lines(warped)
-    result = draw_lane(img, warped, left_fitx, right_fitx, ploty)
+# Define a class to receive the characteristics of each line detection
+class Line:
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False  
+        # x values of the last n fits of the line
+        self.recent_xfitted = [] 
+        #average x values of the fitted line over the last n iterations
+        self.bestx = None     
+        #polynomial coefficients averaged over the last n iterations
+        self.best_fit = None  
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]  
+        #radius of curvature of the line in some units
+        self.radius_of_curvature = None 
+        #distance in meters of vehicle center from the line
+        self.line_base_pos = None 
+        #difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float') 
+        #x values for detected line pixels
+        self.allx = None  
+        #y values for detected line pixels
+        self.ally = None
+
+
+def process_image(img, plot = False):
+    undist = image_processor.undistort(img)
+    thresh = image_processor.thresholding(undist)
+    warped = image_processor.perspective_transform(thresh)
+
+    if left_line.detected == False or right_line.detected == False:
+        left_fit, right_fit = find_lane_lines(warped, plot)
+        #left_line.detected = True
+        #right_line.detected = True
+    else:
+        left_fit, right_fit = find_lane_lines_using_previous_fit(warped,
+                left_line.current_fit, right_line.current_fit, plot)
+
+    left_line.current_fit = left_fit
+    right_line.current_fit = right_fit
+
+    result = draw_lane(img, warped, left_fit, right_fit, plot)
     return result
 
-
-# Read in the saved camera calibration matrix and distortion cofficients
-dist_pickle = pickle.load(open("calibration.p", "rb"))
-mtx = dist_pickle["mtx"]
-dist = dist_pickle["dist"]
-
-# FIXME: pw test color, threshold, and perspective transform
-#example = mpimg.imread('./test_images/test5.jpg');
-#example = mpimg.imread('./test_images/straight_lines1.jpg');
-example = mpimg.imread('./test_images/test4.jpg');
-undist = undistort(example)
-plotTwo((example, undist), ('original', 'undistorted'))
-thresh = thresholding(undist, plot=False)
-plotTwo((undist, thresh), ('undistorted', 'thresholding'), ('', 'gray'))
-
-"""
-h, w = thresh.shape[:2]
-src = np.float32([(575,464), (707,464), (258,682), (1049,682)])
-dst = np.float32([(300,0), (w-300,0), (300,h), (w-300,h)])
-M = cv2.getPerspectiveTransform(src, dst)
-warped = cv2.warpPerspective(thresh, M, (w, h))
-plotTwo((thresh, warped), ('thresholding', 'warped'), ('gray', 'gray'))
-"""
-
-# Get perspective transform matrix
-h, w = undist.shape[:2]
-# Two lines on the "straight_lines1.jpg": (216, 705), (585, 454), (687, 450), (1073, 693)
-slope1 = (705-454)/(216-585);
-slope2 = (450-693)/(687-1073)
-y = 460
-x0 = 198+5
-x4 = 1122
-x1 =  (y-h)/slope1+x0
-x2 = x4 - (h-y)/slope2
-print ("x1: ", x1, ", x2: ", x2)
-
-p_src = np.float32([(x0,h), (x1-3,y), (x2-3,y), (x4,h)])
-p_dst = np.float32([(300,h), (300,0), (w-300,0), (w-300, h)])
-
-p_M = cv2.getPerspectiveTransform(p_src, p_dst)
-
-perspective_transform(undist, plot = True)
-
-warped = perspective_transform(thresh)
-plotTwo((thresh, warped), ('thresholding', 'warped'), ('gray', 'gray'))
-
-find_lane_lines(warped, plot = True)
+image_processor = ImageProcessor()
+left_line = Line()
+right_line = Line()
+#img1 = process_image(mpimg.imread('./test_images/test4.jpg'), plot = True)
+#img2 = process_image(mpimg.imread('./test_images/test5.jpg'), plot = True)
+#quit()
 
 # FIXME: test on all images
-run_all_test_images()
+#run_all_test_images()
+#quit()
 
 # FIXME: on videos
 from moviepy.editor import VideoFileClip
